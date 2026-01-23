e508968fed3bec2dd3bb6189eb7e956db64170fd
Rodrigo Sales <rodrigosalesveloso@gmail.com>
2026-01-22 21:15:30 -0300

    Estilizando texto do header para teste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     return(
         <header className="h-20 bg-gray-800 flex justify-center py-7 gap-2 items-center">
             <img src={ignitelogo} alt="logo" className="h-8 "/>
-            <strong>Ignite Post</strong>
+            <strong className="text-1xl">Ignite Post</strong>
         </header>
         
     )
